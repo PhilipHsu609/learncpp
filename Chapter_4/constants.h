5f5b94f77957bd626fa6394a37a442bbcc75e671
PhilipHsu609 <mpyhacct118113@gmail.com>
2021-02-17 15:36:30 +0800

    add Ch.5

---
+++ Chapter_4/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constant {
+constexpr double pi{3.14159265};
+constexpr double gravity{9.8};
+}  // namespace constant
+
+#endif
+
+// include "constants.h"
+// constant::pi, constant::gravity, etc.
