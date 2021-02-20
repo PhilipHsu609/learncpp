c6144a017b07fc29d4b2c7fd83401f2d0daf24a5
PhilipHsu609 <mpyhacct118113@gmail.com>
2021-02-20 18:13:57 +0800

    change namespace format

--- Chapter_4/constants.h
+++ Chapter_4/constants.h
@@ -2,9 +2,9 @@
 #define CONSTANTS_H
 
 namespace constant {
-constexpr double pi{3.14159265};
-constexpr double gravity{9.8};
-}  // namespace constant
+    constexpr double pi{3.14159265};
+    constexpr double gravity{9.8};
+}
 
 #endif
 
